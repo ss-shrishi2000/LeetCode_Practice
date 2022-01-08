53fb9e6fc0f9900924ca86c8a0231284baff1b39
Srishi__1803 <65821403+ss-shrishi2000@users.noreply.github.com>
2022-01-08 22:58:50 +0530

    Add files via upload

---
+++ Remove_Coloured_Pieces_If_Both_Have_The_Same_Colour.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string colors;
+    cin>>colors;
+    int ca = 0, cb=0, ans1=0, ans2=0;
+      int i=0;
+        while(i<colors.size())
+        {
+            if(colors[i]=='A')
+            {
+                while(i<colors.size() && colors[i]=='A')
+                {
+                    i++;
+                    ca++;
+                }
+
+                if(ca>2)
+                    ans1 += (ca-2), cout<<ca<<" " << ans1 << endl;
+            }
+            else
+            {
+                while(i<colors.size() && colors[i]=='B')
+                {
+                    i++;
+                    cb++;
+                }
+
+                if(cb>2)
+                    ans2 += (cb-2), cout<<cb<<" "<< ans2 <<endl;
+            }
+        }
+
+        cout<< ans1 << " " << ans2 << endl;
+        if(ans1>ans2)
+            cout<<"Alice"<<endl;
+        else
+            cout<<"Bob"<<endl;
+
+        return 0;
+}
