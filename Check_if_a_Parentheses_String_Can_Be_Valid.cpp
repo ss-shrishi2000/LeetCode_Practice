93aff9deb475777812fb6d93ccc8b4e50b9a08f9
Srishi__1803 <65821403+ss-shrishi2000@users.noreply.github.com>
2022-01-24 01:37:55 +0530

    Create Check_if_a_Parentheses_String_Can_Be_Valid.cpp

---
+++ Check_if_a_Parentheses_String_Can_Be_Valid.cpp
@@ -0,0 +1,62 @@
+PROBLEM STATEMENT:- A parentheses string is a non-empty string consisting only of '(' and ')'. It is valid if any of the following conditions is true:
+
+It is ().
+It can be written as AB (A concatenated with B), where A and B are valid parentheses strings.
+It can be written as (A), where A is a valid parentheses string.
+You are given a parentheses string s and a string locked, both of length n. locked is a binary string consisting only of '0's and '1's. For each index i of locked,
+
+If locked[i] is '1', you cannot change s[i].
+But if locked[i] is '0', you can change s[i] to either '(' or ')'.
+Return true if you can make s a valid parentheses string. Otherwise, return false.
+  
+Example 1:
+Input: s = "()()", locked = "0000"
+Output: true
+Explanation: We do not need to make any changes because s is already valid.
+  
+ Example 2:
+Input: s = ")", locked = "0"
+Output: false
+Explanation: locked permits us to change s[0]. 
+Changing s[0] to either '(' or ')' will not make s valid.
+  
+  
+  
+  SOLUTION :-
+    
+    
+    class Solution {
+public:
+    bool canBeValid(string s, string locked) {
+        
+      if (s.size()%2==1) 
+          return false;
+        
+        int bal=0, n=s.size();
+        
+        for (int i=0; i<n; i++)
+        {
+            if (s[i]=='(' || locked[i]=='0')
+                bal++;
+            else 
+                bal--;
+            
+            if (bal<0)
+                return false;
+        }
+        
+        bal = 0;
+        
+        for (int i=n-1; i>=0; i--)
+        {
+            if (s[i]==')' || locked[i]=='0') 
+                bal++;
+            else 
+                bal--;
+            
+            if (bal<0) 
+                return false;
+        }
+        return true;
+    }
+};
