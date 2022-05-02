6ca425731fc0ea19f8885a119570eab3e6d3b53e
Srishi__1803 <65821403+ss-shrishi2000@users.noreply.github.com>
2022-05-02 12:32:22 +0530

    Create Find_Players_With_Zero_Or_One_Losses.cpp

---
+++ Find_Players_With_Zero_Or_One_Losses.cpp
@@ -0,0 +1,62 @@
+PROBLEM STATEMENT :-
+  You are given an integer array matches where matches[i] = [winneri, loseri] indicates that the player winneri defeated player loseri in a match.
+  Return a list answer of size 2 where:
+answer[0] is a list of all players that have not lost any matches.
+answer[1] is a list of all players that have lost exactly one match.
+The values in the two lists should be returned in increasing order.
+
+Note: You should only consider the players that have played at least one match. The testcases will be generated such that no two matches will have the same outcome.
+ 
+Example 1:
+Input: matches = [[1,3],[2,3],[3,6],[5,6],[5,7],[4,5],[4,8],[4,9],[10,4],[10,9]]
+Output: [[1,2,10],[4,5,7,8]]
+Explanation:
+Players 1, 2, and 10 have not lost any matches.
+Players 4, 5, 7, and 8 each have lost one match.
+Players 3, 6, and 9 each have lost two matches.
+Thus, answer[0] = [1,2,10] and answer[1] = [4,5,7,8].
+  
+Example 2:
+Input: matches = [[2,3],[1,3],[5,4],[6,4]]
+Output: [[1,2,5,6],[]]
+Explanation:
+Players 1, 2, 5, and 6 have not lost any matches.
+Players 3 and 4 each have lost two matches.
+Thus, answer[0] = [1,2,5,6] and answer[1] = [].
+  
+  SOLUTION :-
+    class Solution {
+public:
+    vector<vector<int>> findWinners(vector<vector<int>>& matches) 
+    {
+        vector<vector<int>> ans;
+        vector<int> w,l;
+        map<int,int> hash1, hash2, mp1;
+        for(int i=0; i<matches.size(); i++)
+        {
+            int win = matches[i][0], loss = matches[i][1];
+            hash1[win]++;
+            hash2[loss]++;
+            mp1[win]++;
+            mp1[loss]++;
+        }
+        for(auto it=hash2.begin(); it!=hash2.end(); it++)
+        {
+            if(it->second==1)
+                l.push_back(it->first);
+        }
+        for(auto it = mp1.begin(); it!=mp1.end(); it++)
+        {
+            if(hash1.find(it->first)!=hash1.end() && hash1[it->first])
+            {
+                if(it->second == hash1[it->first])
+                    w.push_back(it->first);
+            }
+        }
+        sort(w.begin(), w.end());
+        sort(l.begin(), l.end());
+        ans.push_back(w);
+        ans.push_back(l);
+        return ans;
+    }
+};
